b51a63edeb31344447119f39d307f3a588f08be0
Charlene Gipulan <charlenegipulan@MacBook-Pro.local>
2018-07-18 17:24:24 -0700

    edit reflection

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -1,14 +1,24 @@
 class ReflectionsController < ApplicationController
 
-
     def edit
         @start_date = Date.today.beginning_of_week
         date = params[:id]
         date = date.to_date
         date = date.beginning_of_month
         @reflection = Reflection.find_by(month_date: @date)
-
+        @reflection = Reflection.create(user: current_user, month_date: date) if @reflection.nil?
     end
+
+    def update
+        @reflection = Reflection.find(params[:id]) 
+        @reflection.update_attributes(reflection_params)
+        redirect_to edit_reflection_path("#{@reflection.month_date}")
+    end
+
+    def reflection_params
+        params.require(:reflection).permit(:top10, :learnings, :newthings, :people, :improvements, :progressions, :obstacles, :wrongs)
+    end
+
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
     has_many :entries, dependent: :destroy
+    has_many :reflections, dependent: :destroy
 
 end
